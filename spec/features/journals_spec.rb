183e6524e0e6ddc6b0858345b3dc1cf8667e3226
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-11 14:41:36 -0500

    delete unecessary test files

--- spec/features/journals_spec.rb
+++ spec/features/journals_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature 'Managing journal entries' do
     Journal.create!(event_title: 'Park Hyatt Resort (California)', event_date: 'March 2015')
 
     visit '/journals'
-    expect(page).to have_content 'Journal Entries'
+    expect(page).to have_content 'Welcome to Food adventures'
     expect(page).to have_selector 'ul', count: 1
   end
 
