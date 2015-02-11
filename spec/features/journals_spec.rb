aa9d159aed83da2bd8f8744ce95b156324173866
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-11 15:18:26 -0500

    Create and seed user information for testing purposes

--- spec/features/journals_spec.rb
+++ spec/features/journals_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature 'Managing journal entries' do
+  scenario 'User sign in' do
+    visit '/users/sign_in'
+    fill_in 'user_email', with: 'joe.nolastname@hotmail.com'
+    fill_in 'user_password', with: 'password123'
+    click_button 'Log in'
+    expect(page).to have_content 'Welcome to Food adventures'
+  end
   scenario 'List all journal entries' do
     Journal.create!(event_title: 'Park Hyatt Resort (California)', event_date: 'March 2015')
-
     visit '/journals'
     expect(page).to have_content 'Welcome to Food adventures'
     expect(page).to have_selector 'ul', count: 1
@@ -11,7 +17,6 @@ RSpec.feature 'Managing journal entries' do
 
   scenario 'Create a new journal entry' do
     visit '/journals/new'
-    #save_and_open_page
     fill_in 'journal_event_title', with: 'Park Hyatt Resort (California)'
     fill_in 'journal_event_date', with: 'March 2015'
     click_button 'Create Journal'
@@ -35,6 +40,7 @@ RSpec.feature 'Managing journal entries' do
   scenario 'Delete a Journal entry' do
     journal = Journal.create!(event_title: 'Park Hyatt Resort', event_date: '2015, 1, 12')
     visit "/journals/#{journal.id}"
+    #save_and_open_page
     click_on 'Delete this journal entry'
   end
 end
